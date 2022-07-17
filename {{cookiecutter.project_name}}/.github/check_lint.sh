337e7b29ddc3d450628bf3862d4f32ffb0b7eda4
Alex Thiele <apocthiel@gmail.com>
2022-07-16 23:19:25 -0700

    Better configure vulture and bandit defaults

--- {{cookiecutter.project_name}}/.github/check_lint.sh
+++ {{cookiecutter.project_name}}/.github/check_lint.sh
@@ -6,6 +6,6 @@ poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
 poetry run flake8 {{cookiecutter.project_name}}/ tests/
-poetry run bandit -r {{cookiecutter.project_name}}/
-poetry run vulture {{cookiecutter.project_name}}/
+poetry run bandit -r -c pyproject.toml {{cookiecutter.project_name}}/ tests/
+poetry run vulture --min-confidence 100 {{cookiecutter.project_name}}/ tests/
 echo "Lint successful!"
