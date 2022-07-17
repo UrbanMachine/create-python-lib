da901dcf70f81f695582435827e42d9f7e65afd9
Alex Thiele <apocthiel@gmail.com>
2022-07-17 16:45:28 -0700

    Lower `bandit` sensitivity

--- {{cookiecutter.project_name}}/.github/check_lint.sh
+++ {{cookiecutter.project_name}}/.github/check_lint.sh
@@ -6,6 +6,6 @@ poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
 poetry run flake8 {{cookiecutter.project_name}}/ tests/ --darglint-ignore-regex '^test_.*'
-poetry run bandit -r -c pyproject.toml {{cookiecutter.project_name}}/ tests/
+poetry run bandit -r --severity medium high {{cookiecutter.project_name}}/ tests/
 poetry run vulture --min-confidence 100 {{cookiecutter.project_name}}/ tests/
 echo "Lint successful!"
