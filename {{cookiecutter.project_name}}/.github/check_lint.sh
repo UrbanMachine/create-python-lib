dc49aeb1df23a61960181bb960fcbf3b8d1ea5fb
Alex Thiele <apocthiel@gmail.com>
2022-07-16 23:36:18 -0700

    Add darglint linting

--- {{cookiecutter.project_name}}/.github/check_lint.sh
+++ {{cookiecutter.project_name}}/.github/check_lint.sh
@@ -5,7 +5,7 @@ poetry run cruft check
 poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
-poetry run flake8 {{cookiecutter.project_name}}/ tests/
+poetry run flake8 {{cookiecutter.project_name}}/ tests/ --darglint-ignore-regex '^test_.*'
 poetry run bandit -r -c pyproject.toml {{cookiecutter.project_name}}/ tests/
 poetry run vulture --min-confidence 100 {{cookiecutter.project_name}}/ tests/
 echo "Lint successful!"
